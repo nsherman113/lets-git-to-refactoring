e7206b5d81f23eccffd9d40b82cb558613eee946
nsherman113 <noahsherman113@gmail.com>
2020-12-14 17:53:43 -0600

    console log intro

--- projects/robot-gladiators/assets/js/game.js
+++ projects/robot-gladiators/assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("what is your robot's name?");
+console.log (playerName);
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10+10);
+console.log("Our robot's name is " + playerName);
+
+// fight function // 
+function fight() {
+    window.alert("the fight has begun!");
+}
+//fight();
